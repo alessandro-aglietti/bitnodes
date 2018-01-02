ad74258372f57bcc9242c3d9f2b94b541c73196e
Alessandro Aglietti <alessandro.aglietti@gmail.com>
2018-01-02 14:12:49 +0100

    wip

--- testnet-node-status.py
+++ testnet-node-status.py
@@ -12,11 +12,7 @@ from protocol import (
 )
 
 CONF = {
-    'socket_timeout': 30,
-    'protocol_version': 70015,
-    'services': 0,
     'user_agent': '/bitnodes.earn.com:0.1/',
-    'relay': 0,
     'logfile': 'log/testnet-node-status.log'
 }
 
@@ -29,34 +25,26 @@ def main(argv):
                         format=logformat,
                         filename=CONF['logfile'],
                         filemode='a')
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logging.Formatter(logformat))
+    logging.getLogger().addHandler(consoleHandler)
     print("Log: {}, press CTRL+C to terminate..".format(CONF['logfile']))
 
     address = "87.10.127.99"
     port = 18333
-    height = 5
     to_address = (address, int(port))
-    conn = Connection(to_addr=to_address,
-                      socket_timeout=CONF['socket_timeout'],
-                      proxy=None,
-                      protocol_version=CONF['protocol_version'],
-                      from_services=CONF['services'],
-                      user_agent=CONF['user_agent'],
-                      height=height,
-                      relay=CONF['relay'])
+    conn = Connection(
+        to_addr=to_address,
+        user_agent=CONF['user_agent']
+    )
     try:
         logging.debug("Connecting to %s", conn.to_addr)
         conn.open()
         handshake_msgs = conn.handshake()
-        print("handshake_msgs {}", handshake_msgs)
-
-        # addr_msgs = conn.getaddr()
-        # print("addr_msgs {}", addr_msgs)
-
-        # verack = conn.verack()
-        # print("verack {}", verack)
+        logging.debug("handshake_msgs {}", handshake_msgs)
 
     except (ProtocolError, ConnectionError, socket.error) as err:
-        logging.debug("%s: %s", conn.to_addr, err)
+        logging.error("%s: %s", conn.to_addr, err)
     finally:
         conn.close()
 
